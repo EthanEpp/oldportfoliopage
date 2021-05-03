1c0b46602b8d27e22fb41e8d62bd264ac966bef4
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-05-02 23:35:34 -0700

    Revision To Color Function

--- index.php
+++ index.php
@@ -17,38 +17,43 @@
 </div>
 <div class="flex flex-col text-center items-center justify-end text-sm pl-20 pb-1">Computer Engineer</div></div>
 <!-- <div class="fixed"> -->
-<div class="absolute fixed flex-col top-50 right-0 bg-blue-500 h-20 w-20 mr-8 rounded-sm hover:bg-yellow-100">
+<div class="absolute fixed flex-col top-50 right-0 bg-yellow-100 h-20 w-20 mr-8 rounded-md hover:bg-blue-600">
 <div class="pl-2">
 <div class="text-right font-semibold pr-2 text-red-400" >Contact</div><div class="text-right text-red-400 font-semibold pr-2">About</div><div class="text-right text-red-400 font-semibold pr-2">Home</div>
-</div>
 </div>
 </div>
  
 
 <!-- Idea. Have some sort of enter a code thingy that if you enter a code found on a different page it revails something related to that page secret -->
-<div class="flex font-semibold text-3xl m-5"><span class="text-red-500">I hope you're having a good day:)</span></div>
+<div class="flex font-semibold text-3xl m-5 mt-8"><span class="text-red-500">I hope you're having a good day:)</span></div>
 
-  <div class="flex-col font-medium text-m pt-5">
+  <div class="flex-col font-medium text-m pt-3 pl-5">
   <div>
   <span id="Red" onmouseover="changeColor(this, 'red', '500')" class="transition"  onmouseout="changeColorBack(this, 'red', '500')"><a href="https://www.instagram.com/e.eppiie/">Hello, if you're here you know my name(and if u don't how did u get here?)</a></span>
   </div>
   <div>
-  <span id="Orange" onmouseover="changeColor(this, 'yellow', '500')" class="transition"  onmouseout="changeColorBack(this, 'yellow', '500')"><a href="https://www.instagram.com/e.eppiie/">Wanna learn more about me and what I enjoy? Check it out here!</a></span>
+  <span id="Orange" onmouseover="changeColor(this, 'yellow', '500')" class="transition"  onmouseout="changeColorBack(this, 'yellow', '500')"><a href="http://localhost/Practice-login/PortfolioPage/portfoliopagev1/portfoliopagebio/indexbio.php">Wanna learn more about me and what I enjoy? Check it out here!</a></span>
   </div>
   <div>
-  <span id="Yellow" onmouseover="changeColor(this, 'yellow', '300')" class="transition"  onmouseout="changeColorBack(this, 'yellow', '300')"><a href="https://www.instagram.com/e.eppiie/">What have I done and coded? Check it out here!</a></span>
+  <!-- <span id="Yellow" onmouseover="changeColor(this, 'yellow', '300')" class="transition"  onmouseout="changeColorBack(this, 'yellow', '300')"> -->
+  <a id="Yellow" href="https://github.com/EthanEpp" class="transition" onmouseover="changeColor(this, 'yellow', '300')" onmouseout="changeColorBack(this, 'yellow', '300')">What have I done and coded? Check it out here!</a>
+  <!-- </span> -->
   </div>
   <div>
-  <span id="Green" onmouseover="changeColor(this, 'green', '500')" class="transition"  onmouseout="changeColorBack(this, 'green', '500')"><a href="https://www.instagram.com/e.eppiie/">Wanna have endless drip? Check out my merch here!</a></span>
+  <!-- <span id="Green" onmouseover="changeColor(this, 'green', '500')" class="transition"  onmouseout="changeColorBack(this, 'green', '500')"> -->
+  <a id="Green" href="https://www.instagram.com/e.eppiie/" class="transition" onmouseover="changeColor(this, 'green', '500')" onmouseout="changeColorBack(this, 'green', '500')">Wanna have endless drip? Check out my merch here!</a>
+  <!-- </span> -->
   </div>
   <div>
-  <span id="Blue" onmouseover="changeColor(this, 'blue', '500')" class="transition"  onmouseout="changeColorBack(this, 'blue', '500')"><a href="https://www.instagram.com/e.eppiie/">Wanna see a peek into more daily life? Check out my youtube here!</a></span>
+  <!-- <span id="Blue" onmouseover="changeColor(this, 'blue', '500')" class="transition"  onmouseout="changeColorBack(this, 'blue', '500')"> -->
+  <a id="Blue" href="https://www.youtube.com/channel/UCzunQbjRAKMva7RNPVIM1oQ" class="transition" onmouseover="changeColor(this, 'blue', '500')" onmouseout="changeColorBack(this, 'blue', '500')">Wanna see a peek into more daily life? Check out my youtube here!</a>
+  <!-- </span> -->
   </div>
   <div>
   <a id="Indigo" href="https://www.instagram.com/e.eppiie/" class="transition" onmouseover="changeColor(this, 'indigo', '500')" onmouseout="changeColorBack(this, 'indigo', '500')">Wanna see my public persona? Find me on social media here!</a>
   </div>
   <div>
-  <a id="Purple" href="https://www.instagram.com/e.eppiie/" class="transition"  onmouseover="changeColor(this, 'purple', '500')" onmouseout="changeColorBack(this, 'purple', '500')">Wanna meet up and chat sometime? Schedule a meeting with me here!</a>
+  <a id="Purple" href="https://ethanheyenga.bandcamp.com/track/just-a-memory-of-a-goldfish-ethan-epp" class="transition"  onmouseover="changeColor(this, 'purple', '500')" onmouseout="changeColorBack(this, 'purple', '500')">Wanna listen to some of my music? Find what I have made here!</a>
   </div>
   <div>
   <a id="Pink" href="https://www.instagram.com/e.eppiie/" class="transition"  onmouseover="changeColor(this, 'pink', '500')" onmouseout="changeColorBack(this, 'pink', '500')">Wanna reach out to me somehow? Find my contact information here!</a>
@@ -74,7 +79,7 @@ function changeColor(x, color, num){
 
 function changeColorBack(x,color,num){
     if(y=1){
-    var millisecondsToWait = 1500;
+    var millisecondsToWait = 3000;
     setTimeout(function() {
     y=0;
     x.classList.remove(setColor(color,num));
---
+++ portfoliopagebio/indexbio.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+  </head>
+  
+  <body class="bg-black">
+  <h1 class="text-green-100">hello this is just a temporary bio page because i am testing switching between pages and having different themes on each page.</h1>
+  <div class="text-blue-300">The current plan is to have each page be its own theme but I guess I will find out how well that turns out because im kinda out of ideas rn and kinda tired but it is what it is</div>
+  
+  
+  
+  
+  
+  </body>
